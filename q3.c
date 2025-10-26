2984aaca204bc526151659a471ce3f5166f2d903
devansh sharma <18all38788@gmail.com>
2025-10-26 09:44:39 +0530

    Update q3.c

--- q3.c
+++ q3.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
 int main() {
-    int i;
     int arr[5] = {1};
-    for (i = 0; i < 5; i++)
-        printf("%d", arr[i]);
+    for (int i = 0; i < 5; i++)
+        cout << arr[i];
     return 0;
 }
