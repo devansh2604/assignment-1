64eada5fbf97d0c40ff73c7f9123eeb6c9ac1bb5
devansh sharma <18all38788@gmail.com>
2025-10-26 11:17:36 +0530

    Update q1.c

--- q1.c
+++ q1.c
@@ -1,31 +1,86 @@
 #include <iostream>
 using namespace std;
 
-#define SIZE 100
-
 void create(int arr[], int &n) {
     cout << "Enter number of elements: ";
     cin >> n;
-    cout << "Enter elements:\n";
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
+        cout << "Enter element " << i << ": ";
         cin >> arr[i];
+    }
 }
 
 void display(int arr[], int n) {
-    if (n == 0) {
-        cout << "Array is empty.\n";
-        return;
-    }
-    cout << "Array elements: ";
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
 
-void insert(int arr[], int &n) {
-    int pos, val;
-    cout << "Enter position (0-" << n << "): ";
-    cin >> pos;
-    cout << "Enter value: ";
-    cin >> val;
-    for (int i = n; i >
+void insertElement(int arr[], int &n, int value, int index) {
+    if (index < 0 || index > n) {
+        cout << "Invalid index\n";
+        return;
+    }
+    for (int i = n; i > index; i--)
+        arr[i] = arr[i - 1];
+    arr[index] = value;
+    n++;
+}
+
+int linearSearch(int arr[], int n, int value) {
+    for (int i = 0; i < n; i++)
+        if (arr[i] == value) return i;
+    return -1;
+}
+
+void deleteElement(int arr[], int &n, int value) {
+    int pos = linearSearch(arr, n, value);
+    if (pos != -1) {
+        for (int i = pos; i < n - 1; i++)
+            arr[i] = arr[i + 1];
+        n--;
+    } else {
+        cout << "Element not found!\n";
+    }
+}
+
+int main() {
+    int arr[100], n = 0, choice = 0;
+    while (choice != 6) {
+        cout << "\nMENU\n1.CREATE\n2.DISPLAY\n3.INSERT\n4.DELETE\n5.SEARCH\n6.EXIT\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+        switch (choice) {
+            case 1: create(arr, n); break;
+            case 2: display(arr, n); break;
+            case 3: {
+                int value, index;
+                cout << "Enter value: ";
+                cin >> value;
+                cout << "Enter index: ";
+                cin >> index;
+                insertElement(arr, n, value, index);
+                break;
+            }
+            case 4: {
+                int val;
+                cout << "Enter value to delete: ";
+                cin >> val;
+                deleteElement(arr, n, val);
+                break;
+            }
+            case 5: {
+                int val;
+                cout << "Enter value to search: ";
+                cin >> val;
+                int pos = linearSearch(arr, n, val);
+                if (pos != -1) cout << val << " found at index " << pos << endl;
+                else cout << val << " not found\n";
+                break;
+            }
+            case 6: cout << "Exiting program...\n"; break;
+            default: cout << "Invalid choice!\n";
+        }
+    }
+    return 0;
+}
