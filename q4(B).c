dd4825c3b9f993b64159cab0b7964306d8eb6127
devansh sharma <18all38788@gmail.com>
2025-10-26 09:46:07 +0530

    Convert C code to C++ with iostream

--- q4(B).c
+++ q4(B).c
@@ -1,26 +1,28 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
 int main() {
     int a[10][10], b[10][10], c[10][10];
     int r1, c1, r2, c2;
-    printf("Enter rows and columns of first matrix: ");
-    scanf("%d%d", &r1, &c1);
-    printf("Enter rows and columns of second matrix: ");
-    scanf("%d%d", &r2, &c2);
+    cout << "Enter rows and columns of first matrix: ";
+    cin >> r1 >> c1;
+    cout << "Enter rows and columns of second matrix: ";
+    cin >> r2 >> c2;
 
     if (c1 != r2) {
-        printf("Matrix multiplication not possible.\n");
+        cout << "Matrix multiplication not possible.\n";
         return 0;
     }
 
-    printf("Enter elements of first matrix:\n");
+    cout << "Enter elements of first matrix:\n";
     for (int i = 0; i < r1; i++)
         for (int j = 0; j < c1; j++)
-            scanf("%d", &a[i][j]);
+            cin >> a[i][j];
 
-    printf("Enter elements of second matrix:\n");
+    cout << "Enter elements of second matrix:\n";
     for (int i = 0; i < r2; i++)
         for (int j = 0; j < c2; j++)
-            scanf("%d", &b[i][j]);
+            cin >> b[i][j];
 
     // Initialize result matrix
     for (int i = 0; i < r1; i++)
@@ -33,11 +35,12 @@ int main() {
             for (int k = 0; k < c1; k++)
                 c[i][j] += a[i][k] * b[k][j];
 
-    printf("Resultant matrix:\n");
+    cout << "Resultant matrix:\n";
     for (int i = 0; i < r1; i++) {
         for (int j = 0; j < c2; j++)
-            printf("%d ", c[i][j]);
-        printf("\n");
+            cout << c[i][j] << " ";
+        cout << endl;
     }
+
     return 0;
 }
