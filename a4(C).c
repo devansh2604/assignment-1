86c6c2da9d34c20806e6f7234237fbebad623e4a
devansh sharma <18all38788@gmail.com>
2025-10-26 09:46:48 +0530

    Convert C code to C++ with iostream

--- a4(C).c
+++ a4(C).c
@@ -1,23 +1,26 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
 int main() {
     int a[10][10], transpose[10][10], r, c;
-    printf("Enter rows and columns: ");
-    scanf("%d%d", &r, &c);
+    cout << "Enter rows and columns: ";
+    cin >> r >> c;
 
-    printf("Enter elements:\n");
+    cout << "Enter elements:\n";
     for (int i = 0; i < r; i++)
         for (int j = 0; j < c; j++)
-            scanf("%d", &a[i][j]);
+            cin >> a[i][j];
 
     for (int i = 0; i < r; i++)
         for (int j = 0; j < c; j++)
             transpose[j][i] = a[i][j];
 
-    printf("Transpose matrix:\n");
+    cout << "Transpose matrix:\n";
     for (int i = 0; i < c; i++) {
         for (int j = 0; j < r; j++)
-            printf("%d ", transpose[i][j]);
-        printf("\n");
+            cout << transpose[i][j] << " ";
+        cout << endl;
     }
+
     return 0;
 }
